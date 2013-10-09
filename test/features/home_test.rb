d7581513cd50bb20cab2193b12e4216b74866fc0
NBarnes <nbarnes@gmail.com>
2013-10-09 14:50:56 -0700

    Day 2 assignment, creating new landing page with 'hello world' complete

---
+++ test/features/home_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+feature "Home" do
+  scenario "the test is sound" do
+    visit root_path
+    page.must_have_content "hello world"
+    page.wont_have_content "Goobye All!"
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,3 +17,9 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionDispatch::IntegrationTest
+  include Rails.application.routes.url_helpers
+#  include Capybara::RSpecMatchers
+  include Capybara::DSL
+end
