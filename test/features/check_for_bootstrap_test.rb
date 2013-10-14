7d3b83eb3e6b3435980d8a764ba6563958748566
NBarnes <nbarnes@gmail.com>
2013-10-14 15:46:12 -0700

    Integration of Simple Sidebar Bootstrap theme: http://startbootstrap.com/simple-sidebar

--- test/features/check_for_bootstrap_test.rb
+++ test/features/check_for_bootstrap_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 feature "CheckForBootstrap" do
   scenario "Twitter Bootstrap loaded successfully" do
     visit root_path
-    page.body.must_match /col-/
+    page.body.must_include "bootstrap"
   end
 end
