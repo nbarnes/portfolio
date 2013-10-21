fbec5bdb4d950cffd72201d1c13c13f58b72bcba
NBarnes <nbarnes@gmail.com>
2013-10-21 15:45:57 -0700

    Added title and content validation to post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
+
+  validates :title, :content, presence: true
+  validates :title, length: { in: 2..255 }
+  validates :content, length: { in: 4..512 }
+
 end
