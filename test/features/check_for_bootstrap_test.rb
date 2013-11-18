84eadc220ff4ff9bc67c3c26a23e7bdaba88fa21
NBarnes <nbarnes@gmail.com>
2013-11-18 15:33:18 -0800

    Removed obsolete test for bootstrap installation

--- test/features/check_for_bootstrap_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-feature "CheckForBootstrap" do
-  scenario "Twitter Bootstrap loaded successfully" do
-    visit root_path
-    page.body.must_include "bootstrap"
-  end
-end
