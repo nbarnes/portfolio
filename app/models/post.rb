7ddfa0bde672a63ebdd547bfd64cba272b6ba4a2
NBarnes <nbarnes@gmail.com>
2013-11-24 19:24:39 -0800

    Increased the character limit for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
 
   validates :title, :content, presence: true
   validates :title, length: { in: 2..255 }
-  validates :content, length: { in: 4..5000 }
+  validates :content, length: { in: 4..20000 }
 
   belongs_to :author, class_name: "User"
   has_many :comments, dependent: :destroy
