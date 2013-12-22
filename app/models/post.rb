ee91d1b31616a8b18660ee7ff1e80c8ea41d478e
NBarnes <nbarnes@gmail.com>
2013-12-22 13:28:03 -0800

    Changed the max lengths for strings in Post and Project

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
 
   validates :title, :content, presence: true
   validates :title, length: { in: 2..255 }
-  validates :content, length: { in: 4..20000 }
+  validates :content, length: { in: 4..200000 }
 
   belongs_to :author, class_name: "User"
   has_many :comments, dependent: :destroy, as: :commentable
