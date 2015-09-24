1bf2b49a4c215918ff6df32246d1bfc72d880add
nbarnes <nbarnes@pirq.com>
2015-09-24 15:16:32 -0700

    Gemfile changes, overcommit and rubocop additions, test_helper changes per rubocop's analysis

--- Gemfile
+++ Gemfile
@@ -2,13 +2,12 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 gem 'rails', '3.2.19'
 
-gem 'pg' # PostgreSQL database
-gem 'devise' # authorization
-gem 'minitest-rails'
-gem 'minitest-rails-capybara'
+gem 'pg'
+gem 'jquery-rails'
 gem 'haml'
 gem 'autoprefixer-rails'
 gem 'nestive' # Nested layouts and partials
+gem 'devise' # authentication
 gem 'pundit' # Authorization
 gem 'redcarpet' # Markdown
 gem 'figaro' # secret key hiding
@@ -20,6 +19,8 @@ gem 'twitter'
 
 gem 'whenever' # recurring task running aka cron jobs
 
+# gem 'unicorn'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
@@ -29,26 +30,25 @@ group :assets do
 end
 
 group :production do
-  # For Heroku
   gem 'rails_12factor'
 end
 
 group :development do
   gem 'brakeman', :require => false
   gem 'rails_best_practices'
-  gem 'spring' # Keep rails server running between test runs for speed
-  gem 'launchy' # Allow launching browser from binding.pry
+  gem 'spring'
+  gem 'launchy'
 end
 
 group :development, :test do
+  gem 'minitest-rails'
+  gem 'minitest-rails-capybara'
   gem 'pry-rails'
   gem 'pry-debugger'
-  gem 'capybara-webkit'
 end
 
 group :test do
-  gem 'turn' # prettier test output
   gem 'simplecov' # Test code coverage
+  gem 'capybara-webkit'
+  gem 'headhunter'
 end
-
-gem 'jquery-rails'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,30 +7,27 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
 
 SimpleCov.start 'rails'
 
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../../config/environment", __FILE__)
-require "rails/test_help"
-require "minitest/rails"
-require "minitest/rails/capybara"
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/rails/capybara'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   fixtures :all
   self.use_transactional_fixtures = true
-
-  # Add more helper methods to be used by all tests here...
 
   def login_fox
     visit new_user_session_path
 
-    page.must_have_content "Email"
-    page.must_have_content "Password"
-    page.must_have_content "Remember me"
+    page.must_have_content 'Email'
+    page.must_have_content 'Password'
+    page.must_have_content 'Remember me'
 
-    fill_in "Email", with: users(:fox).email
-    fill_in "Password", with: "foxbarnes"
+    fill_in 'Email', with: users(:fox).email
+    fill_in 'Password', with: 'foxbarnes'
 
-    click_button "Log in"
+    click_button 'Log in'
   end
 
   def logout
@@ -40,9 +37,8 @@ class ActiveSupport::TestCase
 
 end
 
-class ActionDispatch::IntegrationTest
+class ActionDispatchIntegrationTest
   include Rails.application.routes.url_helpers
-#  include Capybara::RSpecMatchers
+  # include Capybara::RSpecMatchers
   include Capybara::DSL
 end
-
