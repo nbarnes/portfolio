61b3d8da5f478a127d7e4da81873d867325d20f1
nbarnes <nbarnes@gmail.com>
2015-10-30 15:24:41 -0700

    Added test (code is self-documenting)

--- test/features/posts/test_posts.rb
+++ test/features/posts/test_posts.rb
@@ -17,6 +17,20 @@ feature 'posts' do
     page.wont_have_content 'post5_title'
   end
 
+  scenario 'new posts are created unpublished' do
+    login_fox
+    visit new_post_path
+    fill_in 'Title', with: 'test post title'
+    fill_in 'Content', with: 'test post content'
+    click_on 'Create Post'
+    page.must_have_content 'Unpublished'
+    page.must_have_content 'test post title'
+    logout
+    visit blog_path
+    page.wont_have_content 'Unpublished'
+    page.wont_have_content 'test post title'
+  end
+
   scenario "Doesn't show projects content when viewing blog content" do
     visit blog_path
     page.wont_have_content 'Portfolio Platform'
