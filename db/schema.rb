87dd259948d9a4011eda743ca14fce62790a61fd
nbarnes <nbarnes@gmail.com>
2015-11-03 17:40:15 -0800

    Migration fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929193042) do
+ActiveRecord::Schema.define(version: 20151104013820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 20150929193042) do
     t.string   "title",                      null: false
     t.string   "content"
     t.string   "link_to"
-    t.string   "screenshot"
     t.boolean  "published",  default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "screenshot"
   end
 
   create_table "users", force: :cascade do |t|
