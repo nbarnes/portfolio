8165ace3bfcc68134516180143701746a42b65a9
nbarnes <nbarnes@gmail.com>
2015-11-07 15:35:09 -0800

    Removed irrelevant type declaration on remove_column

--- db/migrate/20151104013820_change_image_to_screenshot.rb
+++ db/migrate/20151104013820_change_image_to_screenshot.rb
@@ -1,6 +1,6 @@
 class ChangeImageToScreenshot < ActiveRecord::Migration
   def change
     add_column :projects, :screenshot, :string
-    remove_column :projects, :image, :string
+    remove_column :projects, :image
   end
 end
